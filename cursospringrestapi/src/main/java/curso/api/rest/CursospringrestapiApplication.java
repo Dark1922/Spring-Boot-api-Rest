5f3bcd05093a1ea249af849591ecbc08c249187a
dark1922 <jvdematos004@gmail.com>
2021-08-26 14:42:44 -0300

    feath: configurando o projeto.

---
+++ cursospringrestapi/src/main/java/curso/api/rest/CursospringrestapiApplication.java
@@ -0,0 +1,13 @@
+package curso.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringrestapiApplication.class, args);
+	}
+
+}
