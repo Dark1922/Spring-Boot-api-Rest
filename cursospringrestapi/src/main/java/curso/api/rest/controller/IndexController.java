d515b9e764d22f4bea7eff9c98acbf535d1aee3f
dark1922 <jvdematos004@gmail.com>
2021-08-27 17:22:37 -0300

    feath: method put

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,8 +27,8 @@ public class IndexController {
 	// se fosse cdi @Inject
 	private UsuarioRepository usuarioRepository;
 
-	// serviços restfull
-	@GetMapping(value = "/")
+	// serviços restfull  Métodos Buscar por Todos
+	@GetMapping()
 	public ResponseEntity<List<Usuario>> listarTodos() {
 
 		// retorna optional
@@ -35,7 +36,17 @@ public class IndexController {
 
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
+	
+	@GetMapping("/lst")
+	public List<Usuario> listar() {
+		
+		return usuarioRepository.findAll();
+	}
+	
+	
 
+	/*               Métodos de Get buscar por id             */
+	
 	@GetMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
 
@@ -54,8 +65,23 @@ public class IndexController {
 		// o retorno seria um relatorio
 		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
+	
+	@GetMapping(value = "/metodoalex/{id}")
+	public ResponseEntity<Usuario> poridAlex(@PathVariable Long id) {
 
-	@PostMapping("/")
+		// retorna optional
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+		// o retorno seria um relatorio
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	
+	
+	
+	/*               Métodos de Post criação de Usuários             */
+
+	@PostMapping()
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
 
 		Usuario Usuariosalvo = usuarioRepository.save(usuario);
@@ -70,6 +96,26 @@ public class IndexController {
 
 
 		return usuarioRepository.save(usuario);
+
+	}
+	
+	/*               Métodos de Put Atualização de Usuários       */
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Usuario>atualizar(@PathVariable Long id,
+			@RequestBody Usuario usuario) {
+		
+		if(!usuarioRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		usuario.setId(id); //força o usuario a ser atualizado na api
+
+		Usuario UsuarioAtualizar= usuarioRepository.save(usuario);
+
+		return 	ResponseEntity.ok(UsuarioAtualizar);
+		//ou return new ResponseEntity<Usuario>(Usuariosalvo, HttpStatus.OK);
+ 
 
 	}
 }
