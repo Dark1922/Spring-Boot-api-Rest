3feb72c0899e0c50133362669baed14685584856
dark1922 <jvdematos004@gmail.com>
2021-08-28 17:57:28 -0300

    feath: spring security configurando o usuario;

--- cursospringrestapi/src/main/java/curso/api/rest/model/Usuario.java
+++ cursospringrestapi/src/main/java/curso/api/rest/model/Usuario.java
@@ -1,18 +1,28 @@
 package curso.api.rest.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,7 +32,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Usuario  implements Serializable{
+public class Usuario  implements UserDetails{ //ja tem o serializable e os metodos do springsecurity
 
 	private static final long serialVersionUID = 1L;
 
@@ -50,6 +60,52 @@ public class Usuario  implements Serializable{
 	
 	//relacionando com usuario no telefone , removal é pra remover usuario junto com telefone
 	@OneToMany(mappedBy = "usuario" , 
-			orphanRemoval = true, cascade = CascadeType.ALL) 
+			orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY) 
 	private List<Telefone> telefones = new ArrayList<Telefone>();
+	
+    @OneToMany(fetch = FetchType.EAGER) //um usuario pode ter varios papeis
+    @JoinTable(name = "usuarios_role", uniqueConstraints = @UniqueConstraint(
+    		columnNames  = {"usuario_id" , "role_id"} , name = "unique_role_user"),
+    joinColumns = @JoinColumn(name = "usuario_id" , referencedColumnName = "id", table = "usuario", unique = false,
+    foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role", unique = false,updatable = false,
+    foreignKey = @ForeignKey (name = "role_fk", value = ConstraintMode.CONSTRAINT)))
+	private List<Role> roles; //papeis de acesso
+	
+
+	//autorizações são os acessos do usuario ROLE_ADMIN GERENTE etc
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+	
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
