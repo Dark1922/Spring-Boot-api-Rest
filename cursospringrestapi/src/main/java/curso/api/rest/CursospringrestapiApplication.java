a28391cc39cdaf5e916af148e0a7ac77c824a015
dark1922 <jvdematos004@gmail.com>
2021-08-28 15:19:59 -0300

    feath: crossorigin mapeando os acessos.

--- cursospringrestapi/src/main/java/curso/api/rest/CursospringrestapiApplication.java
+++ cursospringrestapi/src/main/java/curso/api/rest/CursospringrestapiApplication.java
@@ -2,12 +2,24 @@ package curso.api.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CursospringrestapiApplication {
+public class CursospringrestapiApplication implements WebMvcConfigurer{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(CursospringrestapiApplication.class, args);
 	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		//libera acesso a todos controlles/endpoints
+		registry.addMapping("usuario/**")
+		.allowedMethods("*")//todos métodos
+		.allowedOrigins("*"); //para todas origens
+	}	
+	//liberando o mapeamento de usuario para todas as origens
 
 }
--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import lombok.AllArgsConstructor;
 @Controller // arquitetura rest
 @RequestMapping("/usuario")
 @AllArgsConstructor
+@CrossOrigin //qlq requisição sera permitido / acessar
 public class IndexController {
 
 	// se fosse cdi @Inject
