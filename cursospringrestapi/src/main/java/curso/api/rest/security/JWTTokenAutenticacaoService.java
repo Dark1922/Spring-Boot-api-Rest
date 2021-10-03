dccb9728de11bef19691dbe46695a9e55754d20f
dark1922 <jvdematos004@gmail.com>
2021-10-02 21:03:26 -0300

    feath: fnlz.

--- cursospringrestapi/src/main/java/curso/api/rest/security/JWTTokenAutenticacaoService.java
+++ cursospringrestapi/src/main/java/curso/api/rest/security/JWTTokenAutenticacaoService.java
@@ -136,7 +136,7 @@ public class JWTTokenAutenticacaoService {
 		
 		if(response.getHeader("Access-Control-Allow-Headers") == null) {
 			response.addHeader("Access-Control-Allow-Headers", "*");
-		}
+		} 
 		
 		if(response.getHeader("Access-Control-Request-Headers") == null) {
 			response.addHeader("Access-Control-Request-Headers", "*");
